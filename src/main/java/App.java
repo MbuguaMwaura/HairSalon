2c3de57fe129a63dcd915cfc29c01652135fd0ba
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-24 18:26:11 +0300

    adds content to the index and form pages using materialize cdn. It also adds a script.js to be able to slide in a side nav bar on smaller devices

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
 
 public class App{
@@ -14,6 +15,16 @@ public class App{
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "templates/index.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/form.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
     }
 }
