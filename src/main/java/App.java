d0cfb9f10f8cedd8f87c14817ce50b49cdcfcb51
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-24 15:51:55 +0300

    adds the imports and basic template of the app.java file

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.staticFileLocation;
+
+public class App{
+    public static void main(String[] args){
+
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+
+    }
+}
+
