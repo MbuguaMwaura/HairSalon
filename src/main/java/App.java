796fd731cbe6df6c9fdaf8bb5a365456e9c0d6b6
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-25 12:30:49 +0300

    adds the routes to the stylists and clients pages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -26,6 +26,17 @@ public class App{
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+        get("/stylists", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/stylists.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/clients", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "template/clients.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 }
 
