546297e2eab751cc93ed3074f469203baed7c3bc
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-25 11:49:13 +0300

    adds a scriptjs file in order to add javascript so as to make the page more interactive

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,4 +1,9 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
+//  document.addEventListener('DOMContentLoaded', function() {
+//    var elems = document.querySelectorAll('.sidenav');
+//    var instances = M.Sidenav.init(elems);
+//  });
+//
+//    document.addEventListener('DOMContentLoaded', function() {
+//      var elems = document.querySelectorAll('.timepicker');
+//      var instances = M.Timepicker.init(elems);
+//    });
