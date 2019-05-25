e6de7991ad71fc61559ecc9b7a6d050f00962775
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-25 17:17:20 +0300

    trys to make the data from the database to be read on the html pages, however there is an error in outputing the data

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,26 +27,48 @@ public class App{
             String gender = request.queryParams("gender");
             Stylist stylist = new Stylist(name,number,age,gender);
             stylist.save();
-
+            model.put("stylist", stylist);
             model.put("stylists", Stylist.all());
             model.put("template", "templates/stylists.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+        post("/clients", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams("stylistId")));
+
+            String name = request.queryParams("clientname");
+            int number = Integer.parseInt(request.queryParams("clientnumber"));
+            String gender = request.queryParams("clientgender");
+
+            String email = request.queryParams("email");
+            Client client = new Client(name, number, email, 1,gender);
+            client.save();
+            model.put("stylists", Stylist.all());
+            model.put("stylist", stylist);
+            model.put("template", "templates/clients.vtl");
+            return new ModelAndView(model,layout);
+        }, new VelocityTemplateEngine());
+
         get("/form", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
+//            Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams("stylistId")));
+            model.put("stylists", Stylist.all());
+//            model.put("stylist", stylist);
             model.put("template", "templates/form.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
         get("/stylists", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
+            model.put("stylists", Stylist.all());
             model.put("template", "templates/stylists.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
         get("/clients", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
+            model.put("clients", Client.all());
             model.put("template", "templates/clients.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -7,3 +7,7 @@
 //      var elems = document.querySelectorAll('.timepicker');
 //      var instances = M.Timepicker.init(elems);
 //    });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
