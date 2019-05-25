7a2e6878a8699cf01ff8b503756dcb752df40cfd
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-25 12:37:27 +0300

    adds a stylist and client page with filler content

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -34,7 +34,7 @@ public class App{
 
         get("/clients", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("template", "template/clients.vtl");
+            model.put("template", "templates/clients.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
     }
