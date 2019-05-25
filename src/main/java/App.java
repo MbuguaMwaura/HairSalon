da9543a8c6fafa6106ada1cb7b474e6009633bb4
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-25 13:22:23 +0300

    adds a sumbit button and post method to the stylists form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,8 +6,7 @@ import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class App{
     public static void main(String[] args){
@@ -18,6 +17,20 @@ public class App{
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "templates/index.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/stylists", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            int number = Integer.parseInt(request.queryParams("number"));
+            int age = Integer.parseInt(request.queryParams("age"));
+            String gender = request.queryParams("gender");
+            Stylist stylist = new Stylist(name,number,age,gender);
+            stylist.save();
+
+            model.put("stylists", Stylist.all());
+            model.put("template", "templates/stylists.vtl");
+            return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
         get("/form", (request, response) -> {
