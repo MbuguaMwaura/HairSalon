f431fdf99094f9b8a1d950ee81908a31e07bf2fa
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-26 17:10:15 +0300

    adds content on to the footer

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,8 +1,8 @@
-//  document.addEventListener('DOMContentLoaded', function() {
-//    var elems = document.querySelectorAll('.sidenav');
-//    var instances = M.Sidenav.init(elems);
-//  });
-//
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
 //    document.addEventListener('DOMContentLoaded', function() {
 //      var elems = document.querySelectorAll('.timepicker');
 //      var instances = M.Timepicker.init(elems);
