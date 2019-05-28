c985922b0c28a79c70d58bf1dd6bd7d0443851e7
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-28 07:34:48 +0300

    makes adjustments in order to make the app look and function well

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,4 +2,8 @@ import org.sql2o.*;
 
 public class DB{
     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "mbugua", "hair");
+
+
+//    public static  Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-163-230-199.compute-1.amazonaws.com:5432/d3c8bc6ksgi6fi", "tnikjohogfxypm", "965dca01e3e6b1131d9ff03ce444c7d2c2151286f4094d641ae23a368af9dd17"); //!
+
 }
---
+++ target/heroku/app/target/classes/public/script.js
@@ -0,0 +1,18 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+//    document.addEventListener('DOMContentLoaded', function() {
+//      var elems = document.querySelectorAll('.timepicker');
+//      var instances = M.Timepicker.init(elems);
+//    });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
+
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.tooltipped');
+      var instances = M.Tooltip.init(elems);
+    });
